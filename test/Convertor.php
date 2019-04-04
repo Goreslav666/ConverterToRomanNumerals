ecc5cb7bb89cb5cfcd1cd5c87e1be21c2178eb4b
lubos <lubos.vlcek@ringier.sk>
2019-04-04 20:01:15 +0200

    Converter json

---
+++ test/Convertor.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lubos.vlcek
+ * Date: 2019-04-04
+ * Time: 17:08
+ */
+
+class Convertor extends \PHPUnit\Framework\TestCase
+{
+
+
+    /**
+     *
+     */
+    public function testConvertsNumbersIntoRoman(){
+        $convertor = new \IW\Controller\Convertor(999);
+        $this->assertEquals('CMXCIX', $convertor->getRoman());
+    }
+
+}
